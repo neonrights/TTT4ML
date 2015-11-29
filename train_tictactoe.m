2c413c38a027a4536d38a73c2b97b5b8b36f057d
neonrights <nicholasfarn@gmail.com>
2015-11-28 18:13:11 -0800

    implementing algorithm

--- train_tictactoe.m
+++ train_tictactoe.m
@@ -1,8 +1,12 @@
 % trains a tic tac toe machine learning algorithm
+% trains it with the assumption the algorithm is going first
 clear all; close all; clc
 
 % constants
 power = 4;
+step = 1e-1;
+threshold = 1e-5;
+max_iter = 1e3;
 
 % get data
 % make sure data is properly formatted
@@ -15,6 +19,29 @@ pX = map_features(X, power);
 n = size(pX, 2);
 
 % init values
-init_a = ones(n, 1);
+corner_a = ones(n , 1);
+corner_Y = Y(:, 1);
+corner_iter = 1;
 
-% get min value using fminunc
+side_a = ones(n, 1);
+side_Y = Y(:, 2);
+side_iter = 1;
+
+middle_a = ones(n, 1);
+middle_Y = Y(:, 5);
+middle_iter = 1;
+
+% get min value for three equations
+[corner_cost, corner_grad] = cost_function(corner_a, pX, corner_Y);
+
+% cost must be below threshold
+while corner_cost > threshold && corner_iter < max_iter
+    % alter a values
+    corner_cost = 1e-6;
+    corner_iter = corner_iter + 1;
+end
+
+disp('final corner values');
+corner_cost
+corner_iter
+sum(abs(corner_a))
