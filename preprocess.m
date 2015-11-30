4c2fa19cc6b9fb962679895f87e5e718a2630b7a
neonrights <nicholasfarn@gmail.com>
2015-11-29 18:37:48 -0800

    edited preprocess
    added more types of mirrors and rotations to the board, plus a comment
    on why I was doing it

--- preprocess.m
+++ preprocess.m
@@ -1,24 +1,63 @@
 function pX = preprocess(X)
-%preprocess rotates the data creating more data to train with
+% preprocess rotates the data creating more data to train with
+% it takes advantage of the fact that a board like so:
+%   x|x|                               | |x     | |   
+%   -+-+-                             -+-+-    -+-+-
+%    | |  is effectively the same as:  | |x or  | |
+%   -+-+-                             -+-+-    -+-+-
+%    | |                               | |     x|x|
+% among other rotations and mirrors
     % rotation matrix
-    rotate = [0 0 0 0 0 0 1 0 0;
-              0 0 0 1 0 0 0 0 0;
-              1 0 0 0 0 0 0 0 0;
-              0 0 0 0 0 0 0 1 0;
-              0 0 0 0 1 0 0 0 0;
-              0 1 0 0 0 0 0 0 0;
-              0 0 0 0 0 0 0 0 1;
-              0 0 0 0 0 1 0 0 0;
-              0 0 1 0 0 0 0 0 0];
+    rotate_90 = [0 0 0 0 0 0 1 0 0;
+                 0 0 0 1 0 0 0 0 0;
+                 1 0 0 0 0 0 0 0 0;
+                 0 0 0 0 0 0 0 1 0;
+                 0 0 0 0 1 0 0 0 0;
+                 0 1 0 0 0 0 0 0 0;
+                 0 0 0 0 0 0 0 0 1;
+                 0 0 0 0 0 1 0 0 0;
+                 0 0 1 0 0 0 0 0 0];
+    % mirror matrices
+    mirror_horizontal = [0 0 0 0 0 0 1 0 0;
+                         0 0 0 0 0 0 0 1 0;
+                         0 0 0 0 0 0 0 0 1;
+                         0 0 0 1 0 0 0 0 0;
+                         0 0 0 0 1 0 0 0 0;
+                         0 0 0 0 0 1 0 0 0;
+                         1 0 0 0 0 0 0 0 0;
+                         0 1 0 0 0 0 0 0 0;
+                         0 0 1 0 0 0 0 0 0];
+    mirror_vertical = [0 0 1 0 0 0 0 0 0;
+                       0 1 0 0 0 0 0 0 0;
+                       1 0 0 0 0 0 0 0 0;
+                       0 0 0 0 0 1 0 0 0;
+                       0 0 0 0 1 0 0 0 0;
+                       0 0 0 1 0 0 0 0 0;
+                       0 0 0 0 0 0 0 0 1;
+                       0 0 0 0 0 0 0 1 0;
+                       0 0 0 0 0 0 1 0 0];
+    % no changes
     pX = X;
     
     % first rotation
-    pX = [pX; X * rotate];
+    pX = [pX; X * rotate_90];
     
     % second rotation
-    pX = [pX; X * rotate * rotate];
+    pX = [pX; X * rotate_90 * rotate_90];
     
     % final rotation
-    pX = [pX; X * rotate * rotate * rotate];
+    pX = [pX; X * rotate_90 * rotate_90 * rotate_90];
+    
+    % mirror horizontal
+    pX = [pX; X * mirror_horizontal];
+    
+    % mirror vertical
+    pX = [pX; X * mirror_vertical];
+    
+    % first diagonal mirror
+    pX = [pX; X * mirror_vertical * rotate_90];
+    
+    % other diagonal mirror
+    pX = [pX; X * mirror_horizontal * rotate_90];
 end
 
